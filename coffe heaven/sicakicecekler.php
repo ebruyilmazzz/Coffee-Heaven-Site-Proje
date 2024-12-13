e14b30ad97c93d5e6dbcbc1bc95198a60709979b
melikekeskin <keskinmelike658@gmail.com>
2024-12-13 18:01:50 +0300

    Update sicakicecekler.php

--- coffe heaven/sicakicecekler.php
+++ coffe heaven/sicakicecekler.php
@@ -2,7 +2,7 @@
 <Html lang="tr">
     <head>
         <meta charset="utf-8">
-        <title>COFFE HEAVEN|SOĞUK İÇECEKLER </title>
+        <title>COFFE HEAVEN|İÇECEKLER </title>
         <link rel="stylesheet" href="css/style2.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
        
@@ -16,67 +16,21 @@
             <div id="logo">COFFE HEAVEN </div>
                 <i><img src="image/WhatsApp Görsel 2024-05-31 saat 14.25.46_f9a883be.jpg" alt="" width="75px"></i>
              <nav>
-             <a href="sogukicecek.php"><i class="fa-solid fa-mug-saucer"></i>Soğuk İçecekler</a>
-                <a href="sicakicecekler.php"><i class="fa-solid fa-mug-hot"></i>Sıcak İçecekler</a>
-                <a href="index.php"><i class="fa-solid fa-house"></i>ANA SAYFA</a>
+             <a href="index.php"><i class="fa-solid fa-house"></i>ANA SAYFA</a>
+                <div class="dropdown">
+            <a href="#" class="dropdown-toggle"><i class="fa-solid fa-mug-saucer"></i>Menüler</a>
+            <div class="dropdown-menu">
+                <a href="#"><i class="fa-solid fa-mug-saucer"></i>İçecekler</a>
+                <a href="#"><i class="fa-solid fa-utensils"></i>Yiyecekler</a>
+            </div>
+             </div>
                 <a href=""><i class="fa-solid fa-user-plus"></i>HESABIM</a>
             </nav>
          </section>
          <section id="egitimler">
             <div class="container">
-                <h3>SICAK İÇECEKLER</h3>
-                <div>                    
-                     <div class="card">
-                    <img src="image/turkish-tea.jpg.webp"
-                    alt="" width="300px"    class="img-fluid">
-                    <h5 class="baslikcard">ÇAY</h5>
-                    <p class="cardp">35 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/turkish .jpg"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">TÜRK KAHVESİ</h5>
-                    <p class="cardp">65 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/sıcakçiko.jpg"
-                    alt="" width="300px"        class="img-fluid">
-                    <h5 class="baslikcard">SICAK ÇİKOLATA</h5>
-                    <p class="cardp">50 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/salep.webp"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">SALEP</h5>
-                    <p class="cardp">55 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/latte.jpg"
-                    alt="" width="300px"     class="img-fluid">
-                    <h5 class="baslikcard">LATTE</h5>
-                    <p class="cardp">65 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/filtre.jpg"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">FİLTRE KAHVE</h5>
-                    <p class="cardp">70 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/macha.jpg"
-                    alt="" width="300px"      class="img-fluid">
-                    <h5 class="baslikcard">MACHA</h5>
-                    <p class="cardp">75 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/expresso.jpg"
-                    alt="" width="300px"      class="img-fluid">
-                    <h5 class="baslikcard">EXPRESSO</h5>
-                    <p class="cardp">80 TL</p>
-                </div>
-               
-            </div>
-
+                <h3>İÇECEKLER</h3>
+                
             </div>
             
         </section>
