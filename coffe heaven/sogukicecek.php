a5ffa6aa934c116b05b7777f65dd4c0f81ef9724
melikekeskin <keskinmelike658@gmail.com>
2024-12-13 18:00:47 +0300

    Update sogukicecek.php

--- coffe heaven/sogukicecek.php
+++ coffe heaven/sogukicecek.php
@@ -2,7 +2,7 @@
 <Html lang="tr">
     <head>
         <meta charset="utf-8">
-        <title>COFFE HEAVEN|SOĞUK İÇECEKLER </title>
+        <title>COFFE HEAVEN|YİYECEKLER</title>
         <link rel="stylesheet" href="css/style2.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
        
@@ -16,71 +16,25 @@
             <div id="logo">COFFE HEAVEN </div>
                 <i><img src="image/WhatsApp Görsel 2024-05-31 saat 14.25.46_f9a883be.jpg" alt="" width="75px"></i>
              <nav>
-                <a href="sogukicecek.php"><i class="fa-solid fa-mug-saucer"></i>Soğuk İçecekler</a>
-                <a href="sicakicecekler.php"><i class="fa-solid fa-mug-hot"></i>Sıcak İçecekler</a>
-                <a href="index.php"><i class="fa-solid fa-house"></i>ANA SAYFA</a>
+             <a href="index.php"><i class="fa-solid fa-house"></i>ANA SAYFA</a>
+                <div class="dropdown">
+            <a href="#" class="dropdown-toggle"><i class="fa-solid fa-mug-saucer"></i>Menüler</a>
+            <div class="dropdown-menu">
+                <a href="#"><i class="fa-solid fa-mug-saucer"></i>İçecekler</a>
+                <a href="#"><i class="fa-solid fa-utensils"></i>Yiyecekler</a>
+            </div>
+             </div>
                 <a href=""><i class="fa-solid fa-user-plus"></i>HESABIM</a>
             </nav>
          </section>
          <section id="egitimler">
             <div class="container">
-                <h3>SOĞUK İÇECEKLER</h3>
-                <div>                    
-                     <div class="card">
-                    <img src="image/soguk-kahve-buzlu-mocha-tarifi-3r3vr5tw.png"
-                    alt="" width="300px"    class="img-fluid">
-                    <h5 class="baslikcard">MOCHA</h5>
-                    <p class="cardp">80 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/iced americano.jpg"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">ICE AMERİCANO</h5>
-                    <p class="cardp">100 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/icedlatte.jpg"
-                    alt="" width="270px"        class="img-fluid">
-                    <h5 class="baslikcard">ICE LATTE</h5>
-                    <p class="cardp">95 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/cold brew.jpg"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">COLD BREW</h5>
-                    <p class="cardp">120 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/limonara (1).png"
-                    alt="" width="300px"     class="img-fluid">
-                    <h5 class="baslikcard">LİMONATA</h5>
-                    <p class="cardp">50 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/churchill.webp"
-                    alt="" width="300px"       class="img-fluid">
-                    <h5 class="baslikcard">CHURCHILL</h5>
-                    <p class="cardp">65 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/PORTAKAL.jpg"
-                    alt="" width="300px"      class="img-fluid">
-                    <h5 class="baslikcard">PORTAKAL SUYU</h5>
-                    <p class="cardp">50 TL</p>
-                </div>
-                <div class="card">
-                    <img src="image/milksake.jpg"
-                    alt="" width="300px"      class="img-fluid">
-                    <h5 class="baslikcard">MLKSHAKE</h5>
-                    <p class="cardp">75 TL</p>
-                </div>
-               
-            </div>
-
+                <h3>YİYECEKLER</h3>
+                
             </div>
             
         </section>
-       
+    
 
     <footer id="footer">
         <h1 class="text-center">COFFE HEAVEN</h1>
@@ -92,4 +46,3 @@
     </footer>
     </body>  
 </Html>
-
