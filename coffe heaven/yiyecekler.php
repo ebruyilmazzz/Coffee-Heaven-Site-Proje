77d85679dafe09af602d0db24257c9e670e88a99
Beyza Yılmaz <beyyzaylmz23@gmail.com>
2024-12-14 14:19:26 +0300

    Create yiyecekler.php

---
+++ coffe heaven/yiyecekler.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<Html lang="tr">
+    <head>
+        <meta charset="utf-8">
+        <title>COFFE HEAVEN|YİYECEKLER</title>
+        <link rel="stylesheet" href="css/style2.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+       
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    </head>
+    <body>
+
+         <section id="menu">
+
+            <div id="logo">COFFE HEAVEN </div>
+                <i><img src="image/WhatsApp Görsel 2024-05-31 saat 14.25.46_f9a883be.jpg" alt="" width="75px"></i>
+             <nav>
+             <a href="index.php"><i class="fa-solid fa-house"></i>ANA SAYFA</a>
+                <div class="dropdown">
+            <a href="#" class="dropdown-toggle"><i class="fa-solid fa-mug-saucer"></i>Menüler</a>
+            <div class="dropdown-menu">
+                <a href="#"><i class="fa-solid fa-mug-saucer"></i>İçecekler</a>
+                <a href="#"><i class="fa-solid fa-utensils"></i>Yiyecekler</a>
+            </div>
+             </div>
+                <a href=""><i class="fa-solid fa-user-plus"></i>HESABIM</a>
+            </nav>
+         </section>
+         <section id="egitimler">
+            <div class="container">
+                <h3>YİYECEKLER</h3>
+                
+            </div>
+            
+        </section>
+    
+
+    <footer id="footer">
+        <h1 class="text-center">COFFE HEAVEN</h1>
+        <p class="text-center">İLETİŞİM<br>
+            0850 308 1552<br>
+            coffeheaven@.com</p>
+        
+      
+    </footer>
+    </body>  
+</Html>
